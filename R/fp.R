0c9791f0e8e5a4a889432fae3e72954e9f6639c9
Rajarshi Guha <rguha@indiana.edu>
2006-09-26 00:00:00 +0000

    version 1.6

--- R/fp.R
+++ R/fp.R
@@ -79,31 +79,37 @@ fpNOff <- function( fp1, fp2, size=1024 ) {
 # original bitstring.
 
 cdk.lf <- function(line) {
-    p <- regexpr("{([0-9,\\s]*)}",line,perl=T)
-    s <- gsub(',','',substr(line, p+1, p+attr(p,"match.length")-2))
-    s <- lapply( strsplit(s,' '), as.numeric )
-    s[[1]]
+  p <- regexpr("{([0-9,\\s]*)}",line,perl=T)
+  s <- gsub(',','',substr(line, p+1, p+attr(p,"match.length")-2))
+  s <- lapply( strsplit(s,' '), as.numeric )
+  s[[1]]
 }
 
 moe.lf <- function(line) {
-    p <- regexpr("\"([0-9\\s]*)\"",line, perl=T)
-    s <- substr(line, p+1, p+attr(p,"match.length")-2)
-    s <- lapply( strsplit(s,' '), as.numeric )
-    s[[1]]
+  p <- regexpr("\"([0-9\\s]*)\"",line, perl=T)
+  s <- substr(line, p+1, p+attr(p,"match.length")-2)
+  s <- lapply( strsplit(s,' '), as.numeric )
+  s[[1]]
+}
+
+bci.lf <- function(line) {
+  tokens <- strsplit(line, '\\s')[[1]]
+  tokens <- tokens[-c(1, length(tokens), length(tokens)-1)]
+  as.numeric(tokens)
 }
 
 fp.read <- function(f='fingerprint.txt', lf=cdk.lf, header=FALSE) {
-    fplist <- list()
-    fcon <- file(description=f,open='r')
-    lines = readLines(fcon,n=-1)
-    if (header) lines = lines[-1]
-    c = 1
-    for (line in lines) {
-        fplist[[c]] <- lf(line)
-        c <- c+1
-    }
-    close(fcon)
-    fplist
+  fplist <- list()
+  fcon <- file(description=f,open='r')
+  lines = readLines(fcon,n=-1)
+  if (header) lines = lines[-1]
+  c = 1
+  for (line in lines) {
+    fplist[[c]] <- lf(line)
+    c <- c+1
+  }
+  close(fcon)
+  fplist
 }
 
 # Need to supply the length of the bit string since fp.read does
